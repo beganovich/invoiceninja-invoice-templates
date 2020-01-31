33f242df774535c566a2b89a570efa1c54f404b6
Benjamin Beganovic <k1pstabug@gmail.com>
2020-01-31 03:16:33 +0100

    004 template

--- source/index.blade.php
+++ source/index.blade.php
@@ -14,6 +14,9 @@
     <a href="templates/003">
         <img src="https://www.invoiceninja.com/wp-content/uploads/2015/03/Invoice-01987-Bold-page-001.jpg" alt="003 template" class="w-64 m-3 border">
     </a>
+    <a href="templates/004">
+        <img src="https://www.invoiceninja.com/wp-content/uploads/2016/10/Invoice-D-085-Plain-page-001.jpg" alt="004 template" class="w-64 m-3 border">
+    </a>
 </div>
 
 @endsection
